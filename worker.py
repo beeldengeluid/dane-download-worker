ae561f714b1486f78d368ae76427523c16a35499
Nanne <n.j.e.vannoord@uva.nl>
2020-03-05 14:25:05 +0100

    Switch to DANE.config

--- worker.py
+++ worker.py
@@ -1,5 +1,4 @@
 import json
-import settings
 from urllib.parse import urlparse
 import urllib.request as req
 from urllib.error import HTTPError
@@ -8,6 +7,7 @@ import shutil
 import os
 
 import DANE.base_classes
+from DANE.config import cfg
 
 
 def parseSize(size, units = 
@@ -22,13 +22,18 @@ class download_worker(DANE.base_classes.base_worker):
     # listen to the same queue
     __queue_name = 'DOWNLOAD'
 
-    def __init__(self, whitelist, config):
+    def __init__(self, config):
         super().__init__(queue=self.__queue_name, 
                 binding_key='#.DOWNLOAD', config=config)
 
-        self.whitelist = whitelist
-        self.search_api = urljoin(config['API'], 'job/search/{}')
-        self.job_api = urljoin(config['API'], 'job/{}')
+        self.whitelist = config.DOWNLOADER.WHITELIST
+        self.search_api = urljoin(config.DANE.API_URL, 'job/search/{}')
+        self.job_api = urljoin(config.DANE.API_URL, 'job/{}')
+
+        self.threshold = None
+        if 'FS_THRESHOLD' in config.DOWNLOADER.keys():
+            # in bytes, might only work on Unix
+            self.threshold = parseSize(config.DOWNLOADER.FS_THRESHOLD)
 
     def callback(self, job):
         parse = urlparse(job.source_url)
@@ -48,13 +53,11 @@ class download_worker(DANE.base_classes.base_worker):
             return json.dumps({'state': 500, 
                 'message': "Non existing TEMP_FOLDER, cannot handle request"})
 
-        if 'FS_THRESHOLD' in self.config.keys():
-            thres_bytes = parseSize(self.config['FS_THRESHOLD'])
-            # this might be Unix only
+        if self.threshold is not None:
             # check if there is enough disk space, to do something meaningful
             disk_stats = os.statvfs(temp_dir)
             bytes_free = statvfs.f_frsize * statvfs.f_bfree 
-            if bytes_free <= thres_bytes:
+            if bytes_free <= self.threshold:
                 # Refuse and requeue for now
                 raise DANE.errors.RefuseJobException('Insufficient disk space')
 
@@ -138,10 +141,8 @@ class download_worker(DANE.base_classes.base_worker):
                 'Content-Length': content_length})
 
 if __name__ == '__main__':
-    config = settings.config
-    rconfig = config['RABBITMQ']
 
-    dlw = download_worker(config['WHITELIST'], config)
+    dlw = download_worker(cfg)
 
     print(' # Initialising worker. Ctrl+C to exit')
     try: 
